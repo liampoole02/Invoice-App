c794895c92f0178192b62fcc64dae8ab86df9866
LIAM <liampoole02@gmail.com>
2021-04-15 15:15:23 +0300

    Product list showing

--- src/App.js
+++ src/App.js
@@ -1,59 +1,38 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Header from './components/header';
 import SideNav from './components/sidenav';
 import Dashboard from './pages/dashboard';
 import Invoice from './pages/invoice';
 import Customer from './pages/customer';
-import Product from './pages/product';
-
-
+import Product from './pages/product/product_list';
+import { Redirect, Route, Switch, BrowserRouter } from 'react-router-dom';
 
 function App() {
-    function getComponent(route) {
-        switch (route) {
-            case "/":
-                return <Dashboard / > ;
-            case "/customer":
-                return <Customer / >
-                    case "/invoice":
-                return <Invoice / >
-                    case "/product":
-                return <Product / >
-                    default:
-                    return null;
-        }
-    }
 
-    function getTitle(route) {
-        switch (route) {
-            case "/":
-                return "Dashboard";
-            case "/customer":
-                return "Customers";
-            case "/invoice":
-                return "Invoices";
-            case "/product":
-                return "Products"
-            default:
-                return null;
-        }
-    }
-    return ( <
-        div className = "wrapper d-flex vh-100 " >
-        <
-        SideNav / >
-        <
-        div className = "main d-flex flex-column flex-grow-1 " >
-        <
-        Header title = { getTitle(window.location.pathname) }
-        /> <
-        div className = "content container-fluid p-4 overflow-auto " > { getComponent(window.location.pathname) } <
-        /div> <
-        /div> <
-        /div >
+  return (
+    <BrowserRouter>
+      <div className="wrapper d-flex vh-100 ">
+        <Route path="/" component={SideNav} />
+        <div className="main d-flex flex-column flex-grow-1 ">
+          <Switch>
+            <Route path="/invoice" render={() => <Header title={"Invoices"} />} />
+            <Route path="/product" render={() => <Header title={"Products"} />} />
+            <Route path="/customer" render={() => <Header title={"Customers"} />} />
+            <Route path="/dashboard" render={() => <Header title={"Dashboard"} />} />
+            <Redirect path="/" to="/dashboard" />
+          </Switch>
+          <div className="content container-fluid p-4 overflow-auto ">
+            <Switch>
+              <Route path="/invoice" component={Invoice} />
+              <Route path="/product" component={Product} />
+              <Route path="/customer" component={Customer} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </div>
+      </div >
+    </BrowserRouter>
 
-    );
+  );
 }
 
 export default App;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,154 +1,128 @@
 function Dashboard() {
-    return ( <
-        >
-        <
-        div className = "row" >
-        <
-        div className = "col-md-6  p-3" >
-        <
-        div className = "card" >
-        <
-        div className = "card-header" >
-        <
-        div className = "lead" > Table < /div>{" "} <
-        /div>{" "} <
-        div className = "card-body" >
-        <
-        table className = "table table-bordered table-hover table-striped" >
-        <
-        thead >
-        <
-        tr >
-        <
-        th > Name < /th> <th> Email </th > { " " } <
-        /tr>{" "} <
-        /thead>{" "} <
-        tbody >
-        <
-        tr >
-        <
-        td > John Smith < /td> <td> john @gmail.com </td > { " " } <
-        /tr>{" "} <
-        tr >
-        <
-        td > Peter Williams < /td> <td> peter @gmail.com </td > { " " } <
-        /tr>{" "} <
-        tr >
-        <
-        td > Jake Tapper < /td> <td> jake @cnn.com </td > { " " } <
-        /tr> <
-        /tbody>{" "} <
-        /table>{" "} <
-        /div>{" "} <
-        /div>{" "} <
-        /div>{" "} <
-        div className = "col-md-6  p-3" >
-        <
-        div className = "card" >
-        <
-        div className = "card-header" >
-        <
-        div className = "lead" > Form < /div>{" "} <
-        /div>{" "} <
-        div className = "card-body" >
-        <
-        div className = "mb-3" >
-        <
-        label htmlFor = "exampleFormControlInput1"
-        className = "form-label" >
-        { " " }
-        Email address { " " } <
-        /label>{" "} <
-        input type = "email"
-        className = "form-control"
-        id = "exampleFormControlInput1"
-        placeholder = "name@example.com" /
-        >
-        <
-        /div>{" "} <
-        div className = "mb-3" >
-        <
-        label htmlFor = "exampleFormControlTextarea1"
-        className = "form-label" >
-        { " " }
-        Example textarea { " " } <
-        /label>{" "} <
-        textarea className = "form-control"
-        id = "exampleFormControlTextarea1"
-        rows = { 3 }
-        defaultValue = { "" }
-        />{" "} <
-        /div>{" "} <
-        div className = "mb-3" >
-        <
-        label htmlFor = "exampleFormControlTextarea1"
-        className = "form-label" >
-        { " " }
-        Example Select { " " } <
-        /label>{" "} <
-        select className = "form-select" >
-        <
-        option value = { 1 } > South Africa < /option>{" "} <
-        option value = { 2 } > Germany < /option>{" "} <
-        option value = { 3 } > U.K. < /option>{" "} <
-        option value = { 4 } > USA < /option>{" "} <
-        option value = { 5 } > Italy < /option>{" "} <
-        /select>{" "} <
-        /div>{" "} <
-        div className = "mb-3 d-flex justify-content-between" >
-        <
-        button type = "submit"
-        className = "btn btn-danger" > { " " }
-        Cancel { " " } <
-        /button>{" "} <
-        button type = "submit"
-        className = "btn btn-primary" > { " " }
-        Submit { " " } <
-        /button>{" "} <
-        /div>{" "} <
-        /div>{" "} <
-        /div>{" "} <
-        /div>{" "} <
-        div className = "col-md-6  p-3" >
-        <
-        div className = "card" >
-        <
-        div className = "card-header" >
-        <
-        div className = "lead" > Table < /div>{" "} <
-        /div>{" "} <
-        div className = "card-body" >
-        <
-        table className = "table table-bordered table-hover table-striped" >
-        <
-        thead >
-        <
-        tr >
-        <
-        th > Name < /th> <th> Email </th > { " " } <
-        /tr>{" "} <
-        /thead>{" "} <
-        tbody >
-        <
-        tr >
-        <
-        td > John Smith < /td> <td> john @gmail.com </td > { " " } <
-        /tr>{" "} <
-        tr >
-        <
-        td > Peter Williams < /td> <td> peter @gmail.com </td > { " " } <
-        /tr>{" "} <
-        tr >
-        <
-        td > Jake Tapper < /td> <td> jake @cnn.com </td > { " " } <
-        /tr> <
-        /tbody>{" "} <
-        /table>{" "} <
-        /div>{" "} <
-        /div>{" "} <
-        /div>{" "} <
-        /div>{" "} <
-        />
+    return (
+        <>
+            <div className="row">
+                <div className="col-md-6  p-3">
+                    <div className="card">
+                        <div className="card-header">
+                            <div className="lead"> Table </div>{" "}
+                        </div>{" "}
+                        <div className="card-body">
+                            <table className="table table-bordered table-hover table-striped">
+                                <thead>
+                                    <tr>
+                                        <th> Name </th> <th> Email </th>{" "}
+                                    </tr>{" "}
+                                </thead>{" "}
+                                <tbody>
+                                    <tr>
+                                        <td> John Smith </td> <td> john @gmail.com </td>{" "}
+                                    </tr>{" "}
+                                    <tr>
+                                        <td> Peter Williams </td> <td> peter @gmail.com </td>{" "}
+                                    </tr>{" "}
+                                    <tr>
+                                        <td> Jake Tapper </td> <td> jake @cnn.com </td>{" "}
+                                    </tr>{" "}
+                                </tbody>{" "}
+                            </table>{" "}
+                        </div>{" "}
+                    </div>{" "}
+                </div>{" "}
+                <div className="col-md-6  p-3">
+                    <div className="card">
+                        <div className="card-header">
+                            <div className="lead"> Form </div>{" "}
+                        </div>{" "}
+                        <div className="card-body">
+                            <div className="mb-3">
+                                <label
+                                    htmlFor="exampleFormControlInput1"
+                                    className="form-label"
+                                >
+                                    {" "}
+                  Email address{" "}
+                                </label>{" "}
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="exampleFormControlInput1"
+                                    placeholder="name@example.com"
+                                />
+                            </div>{" "}
+                            <div className="mb-3">
+                                <label
+                                    htmlFor="exampleFormControlTextarea1"
+                                    className="form-label"
+                                >
+                                    {" "}
+                  Example textarea{" "}
+                                </label>{" "}
+                                <textarea
+                                    className="form-control"
+                                    id="exampleFormControlTextarea1"
+                                    rows={3}
+                                    defaultValue={""}
+                                />{" "}
+                            </div>{" "}
+                            <div className="mb-3">
+                                <label
+                                    htmlFor="exampleFormControlTextarea1"
+                                    className="form-label"
+                                >
+                                    {" "}
+                  Example Select{" "}
+                                </label>{" "}
+                                <select className="form-select">
+                                    <option value={1}> South Africa </option>{" "}
+                                    <option value={2}> Germany </option>{" "}
+                                    <option value={3}> U.K. </option>{" "}
+                                    <option value={4}> USA </option>{" "}
+                                    <option value={5}> Italy </option>{" "}
+                                </select>{" "}
+                            </div>{" "}
+                            <div className="mb-3 d-flex justify-content-between">
+                                <button type="submit" className="btn btn-danger">
+                                    {" "}
+                  Cancel{" "}
+                                </button>{" "}
+                                <button type="submit" className="btn btn-primary">
+                                    {" "}
+                  Submit{" "}
+                                </button>{" "}
+                            </div>{" "}
+                        </div>{" "}
+                    </div>{" "}
+                </div>{" "}
+                <div className="col-md-6  p-3">
+                    <div className="card">
+                        <div className="card-header">
+                            <div className="lead"> Table </div>{" "}
+                        </div>{" "}
+                        <div className="card-body">
+                            <table className="table table-bordered table-hover table-striped">
+                                <thead>
+                                    <tr>
+                                        <th> Name </th> <th> Email </th>{" "}
+                                    </tr>{" "}
+                                </thead>{" "}
+                                <tbody>
+                                    <tr>
+                                        <td> John Smith </td> <td> john @gmail.com </td>{" "}
+                                    </tr>{" "}
+                                    <tr>
+                                        <td> Peter Williams </td> <td> peter @gmail.com </td>{" "}
+                                    </tr>{" "}
+                                    <tr>
+                                        <td> Jake Tapper </td> <td> jake @cnn.com </td>{" "}
+                                    </tr>{" "}
+                                </tbody>{" "}
+                            </table>{" "}
+                        </div>{" "}
+                    </div>{" "}
+                </div>{" "}
+            </div>{" "}
+        </>
     );
 }
 
